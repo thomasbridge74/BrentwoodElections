2181e89f1fd28130a169fb4e49e47df41abf9b5f
Thomas Bridge <tbridge@chicagobooth.edu>
2014-10-21 13:22:28 +0100

    We now have a basic setup that works when run locally.

--- server.R
+++ server.R
@@ -1,5 +1,20 @@
 library(shiny)
+library(plyr)
+
+electiondata <- read.csv("https://raw.githubusercontent.com/thomasbridge74/BrentwoodElections/master/Brentwood%20Elections.csv", header=TRUE)
+years <- sort(unique(electiondata$Year))
+parties <- as.character(sort(unique(electiondata$Party)))
+
+summarize_data <- function(year, party) {    
+  party_vote <- ddply(electiondata[electiondata$Year == year, c("Party", "Vote")], ~Party, summarize, votes = sum(Vote))
+  party_vote$percent <- party_vote$votes/sum(party_vote$votes)
+  party_vote[party_vote$Party %in% party, ]
+}
+
 shinyServer(
   function(input, output) {
+    output$oyear <- renderPrint({input$year})
+    output$oparty <- renderPrint({input$party})
+    output$osummary <- renderPrint({summarize_data(input$year, input$party)})
   }
 )
--- ui.R
+++ ui.R
@@ -1,10 +1,24 @@
 library(shiny)
+
+electiondata <- read.csv("https://raw.githubusercontent.com/thomasbridge74/BrentwoodElections/master/Brentwood%20Elections.csv", header=TRUE)
+years <- sort(unique(electiondata$Year))
+parties <- as.character(sort(unique(electiondata$Party)))
+
 shinyUI(pageWithSidebar(
+  
   headerPanel("Brentwood Elections"),
   sidebarPanel(
-    h3('Select Party/Year')
+    h3('Select Party/Year'),
+    selectInput("year", "Select Year", choices=years),
+    checkboxGroupInput("party", "Parties", choices=parties, selected=parties)
     ),
   mainPanel(
-    h3('Election Results')
+    h3('Main Panel'),
+    verbatimTextOutput("oyear"),
+    h3('Parties'),
+    verbatimTextOutput("oparty"),
+    h3('Vote Summary'),
+    verbatimTextOutput("osummary")
     )
-))
+  )
+)
